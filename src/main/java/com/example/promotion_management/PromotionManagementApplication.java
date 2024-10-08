fea4a68b569b87018459c2779a63e1d1730b1394
emmet hamell <emmethamell@yahoo.com>
2024-10-08 14:59:56 -0400

    initial commit

---
+++ src/main/java/com/example/promotion_management/PromotionManagementApplication.java
@@ -0,0 +1,19 @@
+package com.example.promotion_management;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionManagementApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		//setting application.properties programmatically for database access
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+		SpringApplication.run(PromotionManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/promotion_management/PromotionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promotion_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
