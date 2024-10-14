c4c38af8ce54ae0bcd76c40013873b64926353fe
emmet hamell <emmethamell@yahoo.com>
2024-10-14 16:52:59 -0400

    changes

--- src/main/java/com/example/promotion_management/controller/CouponController.java
+++ src/main/java/com/example/promotion_management/controller/CouponController.java
@@ -1,5 +1,13 @@
 package com.example.promotion_management.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.promotion_management.service.CouponService;
+import com.example.promotion_management.model.Coupon;
 
+@RestController
+@RequestMapping("api/coupons")
 public class CouponController {
     
 }
